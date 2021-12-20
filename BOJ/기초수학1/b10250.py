ae282342862324678040dcc02d9a8f25ddd26fbb
hovelopin <kimhojin3714@naver.com>
2021-12-20 18:23:27 +0900

    BOJ ACM호텔 (기초수학1)

---
+++ BOJ/기초수학1/b10250.py
@@ -0,0 +1,4 @@
+Python 3.9.9 (v3.9.9:ccb0e6a345, Nov 15 2021, 13:06:05) 
+[Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
